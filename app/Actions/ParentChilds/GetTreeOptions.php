ec004ecfa5fc7082cd4ee52b810cebabac1c939e
marco76tv <marco76tv@users.noreply.github.com>
2026-05-06 10:16:12 +0000

    Lint

--- app/Actions/ParentChilds/GetTreeOptions.php
+++ app/Actions/ParentChilds/GetTreeOptions.php
@@ -24,7 +24,8 @@ class GetTreeOptions
     }
 
     /**
-     * @param  Model  $model  - Model that uses HasRecursiveRelationships
+     * @param Model $model - Model that uses HasRecursiveRelationships
+     *
      * @return array<int|string, string>
      */
     public function execute(Model $model): array
