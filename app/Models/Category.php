3330bd3bc2f0add6f534e844d6db742fb5f1db94
marco76tv <marco.sottana@gmail.com>
2026-05-20 07:55:50 +0200

    .

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -25,19 +25,19 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 /**
  * Modules\Blog\Models\Category.
  *
- * @property int                         $id
- * @property string                      $title
- * @property string                      $slug
- * @property Carbon|null                 $created_at
- * @property Carbon|null                 $updated_at
- * @property string|null                 $updated_by
- * @property string|null                 $created_by
- * @property Collection<int, Article>    $articles
- * @property int|null                    $articles_count
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $updated_by
+ * @property string|null $created_by
+ * @property Collection<int, Article> $articles
+ * @property int|null $articles_count
  * @property MediaCollection<int, Media> $media
- * @property int|null                    $media_count
+ * @property int|null $media_count
  *
- * @method static CategoryFactory  factory($count = null, $state = [])
+ * @method static CategoryFactory factory($count = null, $state = [])
  * @method static Builder|Category newModelQuery()
  * @method static Builder|Category newQuery()
  * @method static Builder|Category onlyTrashed()
@@ -82,41 +82,41 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
  * @property int|null                                                              $siblings_and_self_count
  *
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        breadthFirst()
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        depthFirst()
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        doesntHaveChildren()
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category breadthFirst()
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category depthFirst()
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category doesntHaveChildren()
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> get($columns = ['*'])
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        getExpressionGrammar()
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        hasChildren()
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        hasParent()
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        isLeaf()
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        isRoot()
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        tree($maxDepth = null)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        treeOf((Model|callable) $constraint, $maxDepth = null)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereDeletedAt($value)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereDeletedBy($value)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereDepth($operator, $value = null)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereDescription($value)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereLocale(string $column, string $locale)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereLocales(string $column, array $locales)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereParentId($value)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        withGlobalScopes(array $scopes)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        withRelationshipExpression($direction, callable $constraint, $initialDepth, $from = null, $maxDepth = null)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category getExpressionGrammar()
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category hasChildren()
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category hasParent()
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category isLeaf()
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category isRoot()
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category tree($maxDepth = null)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category treeOf((Model|callable) $constraint, $maxDepth = null)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereDeletedAt($value)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereDeletedBy($value)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereDepth($operator, $value = null)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereDescription($value)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereLocale(string $column, string $locale)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereLocales(string $column, array $locales)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereParentId($value)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category withGlobalScopes(array $scopes)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category withRelationshipExpression($direction, callable $constraint, $initialDepth, $from = null, $maxDepth = null)
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> get($columns = ['*'])
  *
- * @property string|null              $icon
- * @property Banner|null              $banner
+ * @property string|null $icon
+ * @property Banner|null $banner
  * @property Collection<int, Article> $categoryArticles
- * @property int|null                 $category_articles_count
+ * @property int|null $category_articles_count
  *
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> get($columns = ['*'])
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereIcon($value)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereIcon($value)
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> get($columns = ['*'])
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereJsonContainsLocale(string $column, string $locale, ?mixed $value)
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereJsonContainsLocales(string $column, array $locales, ?mixed $value)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereJsonContainsLocale(string $column, string $locale, ?mixed $value)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereJsonContainsLocales(string $column, array $locales, ?mixed $value)
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> get($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
@@ -142,7 +142,7 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
  *
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> get($columns = ['*'])
- * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category        whereName($value)
+ * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Builder|Category whereName($value)
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> get($columns = ['*'])
  * @method static \Staudenmeir\LaravelAdjacencyList\Eloquent\Collection<int, static> all($columns = ['*'])
@@ -169,13 +169,13 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
  * @property-read int $depth
  * @property-read string $path
  *
- * @method static Category|null             first()
+ * @method static Category|null first()
  * @method static Collection<int, Category> get()
- * @method static Category                  create(array $attributes = [])
- * @method static Category                  firstOrCreate(array $attributes = [], array $values = [])
- * @method static Builder<static>|Category  where((string|Closure) $column, mixed $operator = null, mixed $value = null, string $boolean = 'and')
- * @method static Builder<static>|Category  whereNotNull((string|Expression) $columns)
- * @method static int                       count(string $columns = '*')
+ * @method static Category create(array $attributes = [])
+ * @method static Category firstOrCreate(array $attributes = [], array $values = [])
+ * @method static Builder<static>|Category where((string|Closure) $column, mixed $operator = null, mixed $value = null, string $boolean = 'and')
+ * @method static Builder<static>|Category whereNotNull((string|Expression) $columns)
+ * @method static int count(string $columns = '*')
  *
  * @property int $is_active
  * @property int $sort_order
@@ -216,7 +216,7 @@ class Category extends BaseModel
 
     public static function getTreeCategoryOptions(): array
     {
-        $instance = new self();
+        $instance = new self;
 
         return app(GetTreeOptions::class)->execute($instance);
     }
