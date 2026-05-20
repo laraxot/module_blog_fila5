05199bb454edd3ddba6da495fa0bdddb6cc7301e
marco76tv <marco.sottana@gmail.com>
2026-05-20 07:55:45 +0200

    .

--- app/tests/Unit/SumTest.php
+++ app/tests/Unit/SumTest.php
@@ -7,7 +7,7 @@ function sum(int|float $a, int|float $b): int|float
     return $a + $b;
 }
 
-it('sum', function (): void {
+it('sum', function(): void {
     $result = sum(1, 2);
 
     expect($result)->toBe(3);
