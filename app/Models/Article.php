2ad7ad27591304f187b3b7f1ae83b10945df78ec
Marco Xot <marco76tv@hotmail.com>
2026-08-31 18:26:32 +0200

    .

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -417,18 +417,6 @@ class Article extends BaseModel implements Feedable, HasTranslationsContract, Su
     }
 
     /**
-     * Scope a query to only include articles different from current article.
-     *
-     * @param EloquentBuilder<Article> $query
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopeDifferentFromCurrentArticle(EloquentBuilder $query, string $currentArticle): EloquentBuilder
-    {
-        return $query->where('id', '!=', $currentArticle);
-    }
-
-    /**
      * The author that belong to the article.
      *
      * @return BelongsTo<Profile, $this>
@@ -541,113 +529,6 @@ class Article extends BaseModel implements Feedable, HasTranslationsContract, Su
     public function getExceptContentBlocks(array $nameBlocks): array
     {
         return app(FilterArticleContentBlocksExceptAction::class)->execute($this, $nameBlocks);
-    }
-
-    /**
-     * Scope a query to only include articles.
-     *
-     * @param EloquentBuilder<Article> $query
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopeArticle(EloquentBuilder $query, string $id): EloquentBuilder
-    {
-        return $query->where('author_id', $id);
-    }
-
-    /**
-     * Scope a query to only include published articles.
-     *
-     * @param EloquentBuilder<Article> $query
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopePublished(EloquentBuilder $query): EloquentBuilder
-    {
-        // return $query->where('status', 'published');
-        // return $query->currentStatus('published');
-        return $query
-            ->whereNotNull('published_at');
-    }
-
-    /**
-     * Scope a query to only include show on homepage articles.
-     *
-     * @param EloquentBuilder<Article> $query
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopeShowHomepage(EloquentBuilder $query): EloquentBuilder
-    {
-        return $query->where('show_on_homepage', 1);
-    }
-
-    /**
-     * Scope a query to only include posted articles until today.
-     *
-     * @param EloquentBuilder<Article> $query
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopePublishedUntilToday(EloquentBuilder $query): EloquentBuilder
-    {
-        return $query->whereDate('published_at', '<=', Carbon::today()->toDateString());
-    }
-
-    /**
-     * Scope a query to only include articles with a specified category.
-     *
-     * @param EloquentBuilder<Article> $query
-     * @param string                   $id    The id of the category
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopeCategory(EloquentBuilder $query, string $id): EloquentBuilder
-    {
-        return $query->where('category_id', $id);
-    }
-
-    /**
-     * Scope a query to only include articles that belongs to an author.
-     *
-     * @param EloquentBuilder<Article> $query
-     * @param string                   $profileId The id of the author
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopeAuthor(EloquentBuilder $query, string $profileId): EloquentBuilder
-    {
-        return $query->where('author_id', $profileId);
-    }
-
-    /**
-     * Scope a query to only include articles with a specified tag.
-     *
-     * @param EloquentBuilder<Article> $query
-     * @param string                   $id    The id of the tag
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopeTag(EloquentBuilder $query, string $id): EloquentBuilder
-    {
-        return $query->whereHas('tags', static function ($q) use ($id): void {
-            $q->where('id', $id);
-        });
-    }
-
-    /**
-     * Scope a query to only include articles which contains searching words.
-     *
-     * @param EloquentBuilder<Article> $query
-     * @param string                   $searching The searching words
-     *
-     * @return EloquentBuilder<Article>
-     */
-    public function scopeSearch(EloquentBuilder $query, string $searching): EloquentBuilder
-    {
-        return $query->where('title', 'LIKE', "%{$searching}%")
-            ->orWhere('content', 'LIKE', "%{$searching}%")
-            ->orWhere('excerpt', 'LIKE', "%{$searching}%");
     }
 
     /**
